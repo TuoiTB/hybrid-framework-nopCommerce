f0c314016abe9cc1448db68d7bb1e789f9a940f1
Từ Bảo Tươi <Tubaotuoi@gmail.com>
2023-07-19 22:18:53 +0700

    Build BasePage Class- Part I

--- actions/commons/BasePage.java
+++ actions/commons/BasePage.java
@@ -1,5 +1,109 @@
 package commons;
 
+import java.util.Set;
+
+import org.openqa.selenium.Alert;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
 public class BasePage {
-	//contains common function for layer page object
+	// contains common function for layer page object
+	// Viết các hàm cho Web Browser
+	public void openUrl(WebDriver driver, String url) {
+		driver.get(url);
+	}
+
+	public String getPageTitle(WebDriver driver) {
+		return driver.getTitle();
+	}
+
+	public String getPageUrl(WebDriver driver) {
+		return driver.getCurrentUrl();
+	}
+
+	public String getPageSource(WebDriver driver) {
+		return driver.getPageSource();
+	}
+
+	public void backToPage(WebDriver driver) {
+		driver.navigate().back();
+	}
+
+	public void refreshCurrentPage(WebDriver driver) {
+		driver.navigate().refresh();
+	}
+
+	public void forwardToPage(WebDriver driver) {
+		driver.navigate().forward();
+	}
+
+	public void acceptToAlert(WebDriver driver) {
+		// driver.switchTo().alert().accept();
+		waitForAlertPresence(driver).accept();
+	}
+
+	public void cancelToAlert(WebDriver driver) {
+		// driver.switchTo().alert().dismiss();
+		waitForAlertPresence(driver).dismiss();
+	}
+
+	public String getAlertText(WebDriver driver) {
+		return waitForAlertPresence(driver).getText();
+	}
+
+	public void senkeyToAlert(WebDriver driver, String textToSenkey) {
+		// driver.switchTo().alert().sendKeys(textToSenkey);
+		waitForAlertPresence(driver).sendKeys(textToSenkey);
+	}
+
+	public Alert waitForAlertPresence(WebDriver driver) {
+		return new WebDriverWait(driver, 30).until(ExpectedConditions.alertIsPresent());
+	}
+
+	// Chỉ dùng cho duy nhất 2 window/tab
+	public void switchToWindowById(WebDriver driver, String windowID) {
+		// windowID = driver.getWindowHandle(); => Id của window mà driver đang đứng
+		Set<String> allIDs = driver.getWindowHandles();
+		for (String id : allIDs) {
+			if (!id.equals(windowID)) {
+				driver.switchTo().window(id);
+				sleepInSecond(2);
+				break;
+			}
+		}
+	}
+
+	// Có thể chạy cho 2 hoặc nhiều hơn 2 window/tab
+	public void switchToWindowByTitle(WebDriver driver, String expectedTitle) {
+		Set<String> allIDs = driver.getWindowHandles();
+		for (String id : allIDs) {
+			driver.switchTo().window(id);
+			String actualTitle = driver.getTitle();
+			if (actualTitle.equals(expectedTitle)) {
+				break;
+			}
+		}
+	}
+
+	// Hàm close hết các window/tab ngoại trừ window/tab cha ban đầu
+	public void closeAllWindowWithoutExpectedID(WebDriver driver, String expectedID) {
+		Set<String> allIDs = driver.getWindowHandles();
+		for (String id : allIDs) {
+			if (!id.equals(expectedID)) {
+				driver.switchTo().window(id);
+				driver.close();
+			}
+		}
+	}
+
+	public void sleepInSecond(long timeout) {
+		try {
+			Thread.sleep(timeout * 1000);
+		} catch (InterruptedException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+
 }
