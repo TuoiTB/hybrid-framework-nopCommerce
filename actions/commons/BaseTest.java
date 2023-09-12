15b6e233e3befba0a7b69e249bfe0f1240b1d5ff
Tu Bao Tuoi <Tubaotuoi@gmail.com>
2023-09-12 14:32:04 +0700

    log4J version 2

--- actions/commons/BaseTest.java
+++ actions/commons/BaseTest.java
@@ -3,8 +3,8 @@ package commons;
 import java.time.Duration;
 import java.util.Random;
 
-import org.apache.commons.logging.Log;
-import org.apache.commons.logging.LogFactory;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.edge.EdgeDriver;
@@ -20,10 +20,10 @@ public class BaseTest {
 	//Contains common function for all testcases
 	//private String projectPath = System.getProperty("user.dir");
 	
-	protected final Log log;
+	protected final Logger log;
 	private WebDriver driver;
 	public BaseTest() {
-		log = LogFactory.getLog(getClass());
+		log = LogManager.getLogger(getClass());
 }
 
 	protected WebDriver getBrowserDriver (String browserName) {
